29b055aa77ab04dbf041743c508c25f3735d6f1f
Michael Bessolov <michael@oroinc.com>
2018-10-15 10:04:09 -0700

    BAP-17728: Fix various typos - Orocommerce -> OroCommerce - BAP -> OroPlatform

--- commerce/conf.py
+++ commerce/conf.py
@@ -245,7 +245,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'theorocommerce', u'The Orocommerce Documentation',
+    ('index', 'theorocommerce', u'The OroCommerce Documentation',
      [u'Oro Team'], 1)
 ]
 
