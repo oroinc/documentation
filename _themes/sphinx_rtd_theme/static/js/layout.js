98bdea73c6600823337a3657018bcbff79713b82
ayerpilov <ayerpilov@magecore.com>
2019-09-23 13:57:14 +0300

    DOC-1367: Version selector is "jumping" when collapsing/expanding ToC.
    - fixed switcher behavior
    - fixed font issue in search results

--- _themes/sphinx_rtd_theme/static/js/layout.js
+++ _themes/sphinx_rtd_theme/static/js/layout.js
@@ -212,20 +212,19 @@ function stickyBlock() {
             var blockSticky = $('.sticky-block'),
                 scrollPos = $(window).scrollTop(),
                 scrollContainer = $('.three-columns-layout'),
-//                  currentPosition = scrollContainer.offset().top + scrollContainer.outerHeight(),
                 footerPos = $('.footer-container').offset().top;
 
             blockSticky.each(function() {
                 var currentSticky = $(this),
-                    currentStickyPaddings = parseInt(currentSticky.css('padding-top')) + parseInt(currentSticky.css('padding-bottom')),
+                    navBox = $(this).find('.sidebar-holder'),
+                    currentStickyPaddings = parseInt(navBox.css('padding-top')) + parseInt(navBox.css('padding-bottom')),
                     sidebarCurrent = currentSticky.parent(),
                     bodyTopPadding = $('.header-container').height(),
-//                    sidebarTopIndent = bodyTopPadding + parseInt(scrollContainer.css('padding-top')) + parseInt(sidebarCurrent.css('padding-top')) + parseInt($('content-container').css('padding-top')),
-                    versionsSwitcher = $('.switcher-container'),
+                    versionsSwitcher = currentSticky.find('.switcher-container'),
                     heightContent = $(window).height() - bodyTopPadding - parseInt(scrollContainer.css('padding-top')) - parseInt($('.content-container').css('padding-top')) - parseInt(scrollContainer.css('padding-bottom')) - parseInt(sidebarCurrent.css('padding-top')) - currentStickyPaddings,
                     heightWithFooter = scrollPos + $(window).height() - footerPos;
 
-                if (currentSticky.next(versionsSwitcher).length > 0) {
+                if (currentSticky.find(versionsSwitcher).length > 0) {
                     heightContent -= (versionsSwitcher.outerHeight() + parseInt(versionsSwitcher.css('margin-top')));
                 }
 
@@ -236,7 +235,6 @@ function stickyBlock() {
                 }
 
                 sidebarCurrent.addClass('sticky-active');
-//            currentSticky.css('top', sidebarTopIndent);
 
                 if (($(window).height() - bodyTopPadding - heightWithFooter - parseInt(scrollContainer.css('padding-top'))) < 150) {
                     if ($('.sidebar').hasClass('v-hidden')) {
@@ -263,16 +261,13 @@ function stickyBlock() {
 function versionSwitcher() {
     var versionsSwitcher = $('.switcher-container');
     if (versionsSwitcher.length > 0) {
-        var blockSticky = versionsSwitcher.prev('.sticky-block'),
-            sidebarTopIndent = blockSticky.outerHeight() + blockSticky.position().top,
+        var switcherParent = versionsSwitcher.closest('.sticky-block'),
             currentPosition;
 
         if ($(window).prop('innerWidth') > 767) {
-            currentPosition = $(window).height() - versionsSwitcher.position().top - versionsSwitcher.height() - parseInt($('.content-container').css('padding-top'));
-            versionsSwitcher.css('top', sidebarTopIndent);
+            currentPosition = $(window).height() - switcherParent.position().top - switcherParent.height() - parseInt($('.content-container').css('padding-top'));
         } else {
             currentPosition = $(window).scrollTop() + $(window).height() - versionsSwitcher.position().top - versionsSwitcher.height() - 10;
-            versionsSwitcher.css('top', 'auto');
         }
 
         if (currentPosition < (versionsSwitcher.find('.documentation-version').outerHeight() + 10)) {
