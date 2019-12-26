8630ea24d63afb1df9501dc8ef014991db1900cc
Ilya Antipenko <iantypenko@oroinc.com>
2019-12-26 13:49:24 +0200

    DOC-1471: Update Documentation Title and robots.txt  - update title suffix

--- conf.py
+++ conf.py
@@ -145,7 +145,7 @@ html_theme_options = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "OroCommerce Documentation"
+html_title = "OroCommerce, OroCRM and OroPlatform Documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
