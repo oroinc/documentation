cf3566439036b56e860ff4b1e7ff0ab8ed60c90a
Mykhailo Sulyma <msulima@swivl.com>
2021-06-01 11:57:12 +0300

    BB-20137: Update documentation about creating a WYSIWYG field
    - added code demo example
    - added behat test to check that fields are in place
    - updated current documentation
    - Updated dev.locks
    | Production Changes               | From    | To      | Compare                                                                              |
    |----------------------------------|---------|---------|--------------------------------------------------------------------------------------|
    | monolog/monolog                  | 1.26.0  | 1.26.1  | [...](https://github.com/Seldaek/monolog/compare/1.26.0...1.26.1)                    |
    | symfony/polyfill-ctype           | v1.22.1 | v1.23.0 | [...](https://github.com/symfony/polyfill-ctype/compare/v1.22.1...v1.23.0)           |
    | symfony/polyfill-iconv           | v1.22.1 | v1.23.0 | [...](https://github.com/symfony/polyfill-iconv/compare/v1.22.1...v1.23.0)           |
    | symfony/polyfill-intl-icu        | v1.22.1 | v1.23.0 | [...](https://github.com/symfony/polyfill-intl-icu/compare/v1.22.1...v1.23.0)        |
    | symfony/polyfill-intl-idn        | v1.22.1 | v1.23.0 | [...](https://github.com/symfony/polyfill-intl-idn/compare/v1.22.1...v1.23.0)        |
    | symfony/polyfill-intl-normalizer | v1.22.1 | v1.23.0 | [...](https://github.com/symfony/polyfill-intl-normalizer/compare/v1.22.1...v1.23.0) |
    | symfony/polyfill-mbstring        | v1.22.1 | v1.23.0 | [...](https://github.com/symfony/polyfill-mbstring/compare/v1.22.1...v1.23.0)        |
    | symfony/polyfill-php72           | v1.22.1 | v1.23.0 | [...](https://github.com/symfony/polyfill-php72/compare/v1.22.1...v1.23.0)           |
    | symfony/polyfill-php73           | v1.22.1 | v1.23.0 | [...](https://github.com/symfony/polyfill-php73/compare/v1.22.1...v1.23.0)           |

---
+++ code_examples/commerce/entity_field/wysiwyg/ACMEWysiwygBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ACME\Bundle\WysiwygBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * The WysiwygBundle bundle class.
+ */
+class ACMEWysiwygBundle extends Bundle
+{
+}
