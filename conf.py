0234db96808cb79d4a4dcd1a57e2c81a59643714
Ivan Makarenkov <imakarenkov@oroinc.com>
2021-08-13 12:55:31 +0300

    fix

--- conf.py
+++ conf.py
@@ -93,7 +93,7 @@ copyright = u'2017, Oro Team'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files. , 'backend', 'frontend', 'community', 'developer', 'user'
-exclude_patterns = ['_build', 'completeReference/overview', 'sphinx', 'docker', '_themes', 'bundles']
+exclude_patterns = ['_build', 'completeReference/overview', 'sphinx', 'docker', '_themes']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
