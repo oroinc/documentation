3af15dc963a08d7ac589d3c9f3f0f0a9dcb3b8a2
Andrey Yatsenko <yatsenco@gmail.com>
2021-08-14 22:57:19 +0300

    removed docker folder

--- conf.py
+++ conf.py
@@ -93,7 +93,7 @@ copyright = u'2017, Oro Team'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'completeReference/overview', 'sphinx', 'docker', '_themes']
+exclude_patterns = ['_build', 'completeReference/overview', 'sphinx', '_themes']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
