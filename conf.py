158b6d5e3cbbf69ef7f96ad340ba91b49b3013f9
Andrey Yatsenko <yatsenco@gmail.com>
2021-08-14 19:28:05 +0300

    minor fixes

--- conf.py
+++ conf.py
@@ -92,7 +92,7 @@ copyright = u'2017, Oro Team'
 #today_fmt = '%B %d, %Y'
 
 # List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files. , 'backend', 'frontend', 'community', 'developer', 'user'
+# directories to ignore when looking for source files.
 exclude_patterns = ['_build', 'completeReference/overview', 'sphinx', 'docker', '_themes']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
