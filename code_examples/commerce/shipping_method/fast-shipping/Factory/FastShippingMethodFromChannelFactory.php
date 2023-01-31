9cae074b0a0a94c468ba65e1b5c6e63c5b8b8c7b
Vova Soroka <vsoroka@oroinc.com>
2023-01-31 18:02:28 +0200

    BB-21884: Refactor shipping method factories (#34758)
    - add strict types to Oro\Bundle\ShippingBundle\Method\Factory\IntegrationShippingMethodFactoryInterface and all classes that implement this interface
    - simplify code of all classes that implement Oro\Bundle\ShippingBundle\Method\Factory\IntegrationShippingMethodFactoryInterface
    - add strict types to Oro\Bundle\IntegrationBundle\Generator\IntegrationIdentifierGeneratorInterface and all classes that implement this interface

--- code_examples/commerce/shipping_method/fast-shipping/Factory/FastShippingMethodFromChannelFactory.php
+++ code_examples/commerce/shipping_method/fast-shipping/Factory/FastShippingMethodFromChannelFactory.php
@@ -10,36 +10,19 @@ use Oro\Bundle\IntegrationBundle\Generator\IntegrationIdentifierGeneratorInterfa
 use Oro\Bundle\IntegrationBundle\Provider\IntegrationIconProviderInterface;
 use Oro\Bundle\LocaleBundle\Helper\LocalizationHelper;
 use Oro\Bundle\ShippingBundle\Method\Factory\IntegrationShippingMethodFactoryInterface;
+use Oro\Bundle\ShippingBundle\Method\ShippingMethodInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
- * Factory that creates shipping method from the channel
+ * The factory to create Fast Shipping method.
  */
 class FastShippingMethodFromChannelFactory implements IntegrationShippingMethodFactoryInterface
 {
-    /**
-     * @var IntegrationIdentifierGeneratorInterface
-     */
-    private $identifierGenerator;
+    private IntegrationIdentifierGeneratorInterface $identifierGenerator;
+    private LocalizationHelper $localizationHelper;
+    private TranslatorInterface $translator;
+    private IntegrationIconProviderInterface $integrationIconProvider;
 
-    /**
-     * @var LocalizationHelper
-     */
-    private $localizationHelper;
-
-    /**
-     * @var TranslatorInterface
-     */
-    private $translator;
-
-    /**
-     * @var IntegrationIconProviderInterface
-     */
-    private $integrationIconProvider;
-
-    /**
-     * FastShippingMethodFromChannelFactory constructor.
-     */
     public function __construct(
         IntegrationIdentifierGeneratorInterface $identifierGenerator,
         LocalizationHelper $localizationHelper,
@@ -52,38 +35,33 @@ class FastShippingMethodFromChannelFactory implements IntegrationShippingMethodF
         $this->integrationIconProvider = $integrationIconProvider;
     }
 
-    public function create(Channel $channel): FastShippingMethod
-    {
-        $id = $this->identifierGenerator->generateIdentifier($channel);
-        $label = $this->getChannelLabel($channel);
-        $icon = $this->getIcon($channel);
-        $types = $this->createTypes();
-
-        return new FastShippingMethod($id, $label, $icon, $channel->isEnabled(), $types);
-    }
-
-    private function getChannelLabel(Channel $channel): string
+    /**
+     * {@inheritDoc}
+     */
+    public function create(Channel $channel): ShippingMethodInterface
     {
         /** @var FastShippingSettings $transport */
         $transport = $channel->getTransport();
 
-        return (string) $this->localizationHelper->getLocalizedValue($transport->getLabels());
-    }
-
-    private function getIcon(Channel $channel): ?string
-    {
-        return $this->integrationIconProvider->getIcon($channel);
+        return new FastShippingMethod(
+            $this->identifierGenerator->generateIdentifier($channel),
+            (string)$this->localizationHelper->getLocalizedValue($transport->getLabels()),
+            $this->integrationIconProvider->getIcon($channel),
+            $channel->isEnabled(),
+            $this->createTypes()
+        );
     }
 
     private function createTypes(): array
     {
-        $withoutPresentLabel = $this->translator
-            ->trans('acme.fast_shipping.method.processing_type.without_present.label');
-        $withPresentLabel = $this->translator
-            ->trans('acme.fast_shipping.method.processing_type.with_present.label');
-
-        $withoutPresent = new FastShippingMethodType($withoutPresentLabel, false);
-        $withPresent = new FastShippingMethodType($withPresentLabel, true);
+        $withoutPresent = new FastShippingMethodType(
+            $this->translator->trans('acme.fast_shipping.method.processing_type.without_present.label'),
+            false
+        );
+        $withPresent = new FastShippingMethodType(
+            $this->translator->trans('acme.fast_shipping.method.processing_type.with_present.label'),
+            true
+        );
 
         return [
             $withoutPresent->getIdentifier() => $withoutPresent,
