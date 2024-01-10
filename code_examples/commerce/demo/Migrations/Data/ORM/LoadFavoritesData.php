2c01b91daf57941007b4547b16071b5cae9d6822
ikhrysieva <103260327+ikhrysieva@users.noreply.github.com>
2024-01-10 13:03:51 +0100

    BAP-22404: Fix documentation examples. AbstractFixture (#37310)

--- code_examples/commerce/demo/Migrations/Data/ORM/LoadFavoritesData.php
+++ code_examples/commerce/demo/Migrations/Data/ORM/LoadFavoritesData.php
@@ -3,8 +3,8 @@
 namespace Acme\Bundle\DemoBundle\Migrations\Data\ORM;
 
 use Acme\Bundle\DemoBundle\Entity\Favorite;
+use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Persistence\ObjectManager;
-use Oro\Bundle\DotmailerBundle\Tests\Functional\Fixtures\AbstractFixture;
 use Oro\Bundle\MigrationBundle\Fixture\VersionedFixtureInterface;
 use Oro\Bundle\OrganizationBundle\Entity\Organization;
 
